cb63fc9416596f2e12a2c058726a35d9f0280812
Justin Comins <jcomins000@gmail.com>
2023-02-12 00:53:33 -0800

    Updates

--- src.77de5100.js
+++ src.77de5100.js
@@ -194,7 +194,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57752" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51910" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
